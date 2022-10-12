f09ccf0f1ee351dd287cf710bc8f35c08712d257
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2022-10-12 11:23:47 +0100

    feat: adding bubble sort

--- tp1/ex1.c
+++ tp1/ex1.c
@@ -16,7 +16,7 @@ int main() {
     *(arr + i) = rand();
 
     printf("%d, ", *(arr + i));
-  }
+  } 
 
   max = *arr;
 
--- tp1/ex2.c
+++ tp1/ex2.c
@@ -1,15 +1,48 @@
-#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-int chercheDicht(int arr[], int n, int cle) {
-  int d = 1;
-  int f = n;
-  int trouve = false;
+void sort(int *arr, int n) {
+  int i, j, temp;
+
+  for (i = 0; i < n; i++) {
+    for (j = i + 1; j < n; j++) {
+      if (*(arr + i) > *(arr + j)) {
+        temp = *(arr + i);
+        *(arr + i) = *(arr + j);
+        *(arr + j) = temp;
+      }
+    }
+  }
+}
+
+void printArr(int *arr, int n) {
+  int *p;
+
+  for (p = arr; p <= arr + n - 1; p++) {
+    printf("%d, ", *p);
+  }
 }
 
 int main() {
-  int n, i, max, *arr;
+  int n, i, *arr;
+
+  printf("N: ");
+  scanf("%d: ", &n);
+
+  for (i = 0; i < n; i++) {
+    printf("arr[%d]: ", i);
+    scanf("%d", &arr[i]);
+  }
+
+  printf("Previous array: ");
+
+  printArr(arr, n);
+
+  sort(arr, n);
+
+  printf("\nSorted array: ");
+
+  printArr(arr, n);
 
   return 0;
 }
