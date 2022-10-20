56d4a547e431d4db7ad901b3b84062100894b48e
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2022-10-20 20:58:21 +0100

    feat: adding all functions

--- tp3/mainliste.c
+++ tp3/mainliste.c
@@ -3,16 +3,66 @@
 
 #include "liste.h"
 
+int menu() {
+  printf("\n\n GESTION D'UNE LISTE D'ENTIERS \n\n");
+  printf("1 - Creer une liste\n");
+  printf("2 - Insertion en tete de liste \n");
+  printf("3 - Insertion en fin de liste \n");
+  printf("4 - Retrait en tete de liste \n");
+  printf("5 - Retrait en fin de liste \n");
+  printf("6 - Retrait d’un objet à partir de sa reference\n");
+  printf("7 - Afficher les objets de la liste \n");
+  printf("8 - Chercher Un objet \n");
+  printf("9 - Destruction de la liste \n");
+  printf("10 - Fin\n");
+  printf("\n");
+  printf("Votre choix ? ");
+
+  int code;
+
+  scanf("%d", &code);
+  getchar();
+  printf("\n");
+
+  return code;
+}
+
 int main() {
-  struct node* head = create_list(45, 90);
+  struct node* head = NULL;
+  int code = menu();
 
-  insert_at_end(head, 80);
-  insert_at_end(head, 100);
-  insert_at_end(head, 200);
+  while (1) {
+    switch (code) {
+      case 1:
+        int num1, num2;
 
-  destroy_list(&head);
+        printf("\nEnter 2 numbers: ");
+        scanf("%d %d", &num1, &num2);
 
-  print_list_data(head);
+        head = create_list(num1, num2);
+
+        printf("\nCreated list: ");
+        print_list_data(head);
+        code = menu();
+        break;
+      case 2:
+        int num;
+
+        printf("\nEnter a number to insert: ");
+        scanf("%d", &num);
+
+        struct node* new_head = insert_at_beginning(head, num);
+
+        head = new_head;
+
+        printf("\nList after insert: ");
+        print_list_data(head);
+        code = menu();
+        break;
+      default:
+        break;
+    }
+  }
 
   return 0;
 }
