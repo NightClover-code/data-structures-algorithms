7d2690975763874cbca43d3440498b778a6d805d
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2022-11-02 22:32:13 +0100

    fix: small fix

--- tp4/listePersonne.c
+++ tp4/listePersonne.c
@@ -6,14 +6,14 @@
 int menu() {
   printf("\n\n GESTION D'UNE LISTE D'ENTIERS \n\n");
   printf("0 - Fin\n");
-  printf("2 - Insertion en tete de liste \n");
-  printf("3 - Insertion en fin de liste \n");
-  printf("4 - Retrait en tete de liste \n");
-  printf("5 - Retrait en fin de liste \n");
-  printf("6 - Retrait d’un element à partir de son nom\n");
-  printf("7 - Parcours de la liste \n");
-  printf("8 - Recherche d’un element à partir de son nom \n");
-  printf("9 - Destruction de la liste \n");
+  printf("1 - Insertion en tete de liste \n");
+  printf("2 - Insertion en fin de liste \n");
+  printf("3 - Retrait en tete de liste \n");
+  printf("4 - Retrait en fin de liste \n");
+  printf("5 - Retrait d’un element à partir de son nom\n");
+  printf("6 - Parcours de la liste \n");
+  printf("7 - Recherche d’un element à partir de son nom \n");
+  printf("8 - Destruction de la liste \n");
   printf("\n");
   printf("Votre choix ? ");
 
@@ -26,6 +26,8 @@ int menu() {
 void main() {
   Liste* lp = creer_liste(0, afficher_personne, comparer_personne);
   bool fini = false;
+  Personne *p, *pp, *extrait, *personne_cherche;
+  ch15 nom, prenom;
   while (!fini) {
     switch (menu()) {
       case 0:
@@ -33,26 +35,22 @@ void main() {
         break;
       case 1:
         printf("Nom de la personne: ");
-        ch15 nom;
         scanf("%s", &nom);
         printf("Prenom de la personne: ");
-        ch15 prenom;
         scanf("%s", &prenom);
-        Personne* p = cree_personne(nom, prenom);
+        p = cree_personne(nom, prenom);
         inserer_en_tete(lp, p);
         break;
       case 2:
         printf("Nom de la personne: ");
-        ch15 nom;
         scanf("%s", &nom);
         printf("Prenom de la personne: ");
-        ch15 prenom;
         scanf("%s", &prenom);
-        Personne* p = cree_personne(nom, prenom);
+        p = cree_personne(nom, prenom);
         inserer_en_fin(lp, p);
         break;
       case 3:
-        Personne* extrait = (Personne*)extraire_en_tete(lp);
+        extrait = (Personne*)extraire_en_tete(lp);
         if (extrait != NULL) {
           printf("Element %s %s extrait de la liste!", extrait->nom,
                  extrait->prenom);
@@ -61,7 +59,7 @@ void main() {
         }
         break;
       case 4:
-        Personne* extrait = (Personne*)extraire_fin(lp);
+        extrait = (Personne*)extraire_fin(lp);
         if (extrait != NULL) {
           printf("Element %s %s extrait de la liste!", extrait->nom,
                  extrait->prenom);
@@ -70,10 +68,9 @@ void main() {
         }
       case 5:
         printf("Nom de la personne a extraire: ");
-        ch15 nom;
         scanf("%s", &nom);
-        Personne* personne_cherche = cree_personne(nom, "?");
-        Personne* pp = (Personne*)chercher_objet(lp, personne_cherche);
+        personne_cherche = cree_personne(nom, "?");
+        pp = (Personne*)chercher_objet(lp, personne_cherche);
         if (extraire_objet(lp, pp)) {
           printf("Element %s %s extrait de la liste!", pp->nom, pp->prenom);
         } else {
@@ -85,10 +82,9 @@ void main() {
         break;
       case 7:
         printf("Nom de la personne a rechercher: ");
-        ch15 nom;
         scanf("%s", &nom);
-        Personne* personne_cherche = cree_personne(nom, "?");
-        Personne* pp = (Personne*)chercher_objet(lp, personne_cherche);
+        personne_cherche = cree_personne(nom, "?");
+        pp = (Personne*)chercher_objet(lp, personne_cherche);
         if (pp != NULL) {
           printf("%s %s trouvé dans la liste!", pp->nom, pp->prenom);
         } else {
